28b29e4d5e578bb9d53c7457565e85d5e3a51948
lazy lizard <lazy_lizard@github.com>
2020-08-20 22:42:45 +0530

    Updated the Display message in React
    Changing the commiter

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          //Edit <code>src/App.js</code> and save to reload.
           Hi , There 
+	Welcome to First Page of React build on docker. 
         </p>
         <a
           className="App-link"
